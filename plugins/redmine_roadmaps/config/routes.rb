f559261c734410cfb4fbbd1d91193f389f692222
Sleyder Zuleta <sleyderzm1@gmail.com>
2018-05-05 20:19:54 -0500

    fix reoadmap routes

--- plugins/redmine_roadmaps/config/routes.rb
+++ plugins/redmine_roadmaps/config/routes.rb
@@ -1,3 +1,3 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect "/roadmaps", :controller => "roadmaps_main", :action => "index", :conditions => { :method => :get}
+RedmineApp::Application.routes.draw do
+  match "/roadmaps", :controller => "roadmaps_main", :action => "index", :via => :get
 end
