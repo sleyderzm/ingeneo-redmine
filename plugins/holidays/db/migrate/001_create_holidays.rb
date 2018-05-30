59bca7208f9b77ebab07eb2360a1705f3937a619
Sleyder Zuleta <sleyderzm1@gmail.com>
2018-05-30 00:18:09 -0500

    backups and fix

--- plugins/holidays/db/migrate/001_create_holidays.rb
+++ plugins/holidays/db/migrate/001_create_holidays.rb
@@ -1,11 +1,5 @@
 class CreateHolidays < ActiveRecord::Migration
   def change
-    create_table :holidays do |t|
-      t.date :date
-      t.text :description
-	  t.integer :year
-      t.datetime :created_on
-      t.datetime :updated_on
-    end
+    add_column :holidays, :year, :integer
   end
 end
